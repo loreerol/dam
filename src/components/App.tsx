41ae70ffca390b7d64e9aef1a872079336a6102d
loreerol <lorehill.hello@gmail.com>
2025-03-26 15:46:24 -0400

    initialised with create-react-app and set up MUI

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" gutterBottom>
+        DAM, what great content!
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
